42df95b5494aedab5580e38718dec2059420d1c6
Carlos_Lima <carloslima@icloud.com>
2024-01-06 20:41:24 -0300

    .

---
+++ cursojs/aula05/basic-funtion.js
@@ -0,0 +1,8 @@
+var nome = window.prompt("Qual é o seu nome?");
+window.alert("É um prazer em te conhecer, " + nome + "!");
+
+var n1 = Number(window.prompt("Digite um número " + nome + ":"));
+var n2 = Number(window.prompt("Digite outro número " + nome + ":"));
+var s = n1 + n2;
+window.alert(
+  `, o resultado da soma é " + s + ". Viu como sou inteligente " + nome + "?"
