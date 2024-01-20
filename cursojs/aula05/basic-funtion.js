e1d98486b7246e9a13335f6b0e48ad15bb98b50e
Carlos_Lima <carloslima@icloud.com>
2024-01-20 16:38:06 -0300

    .

--- cursojs/aula05/basic-funtion.js
+++ cursojs/aula05/basic-funtion.js
@@ -5,4 +5,4 @@ var n1 = Number(window.prompt("Digite um número " + nome + ":"));
 var n2 = Number(window.prompt("Digite outro número " + nome + ":"));
 var s = n1 + n2;
 window.alert(
-  `, o resultado da soma é " + s + ". Viu como sou inteligente " + nome + "?"
+  `, o resultado da soma é " + s + ". Viu como sou inteligente " + nome + "?";
